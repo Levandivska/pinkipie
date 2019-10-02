0627c03b0bc6e91c5fa85af8fca40c7458b07908
Taras Mazurkevych <mad.taras@gmail.com>
2019-10-02 15:07:04 +0300

    set up repo

---
+++ pinkipie/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  pinkipie
+//
+//  Created by Taras Mazurkevych on 10/2/19.
+//  Copyright © 2019 pinkipie. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
