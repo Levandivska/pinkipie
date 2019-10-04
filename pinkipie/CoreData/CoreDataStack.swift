529f13105eeffdc951bf1361b07de5cce345fba7
Anastasia <traverse@ucu.edu.ua>
2019-10-04 21:12:59 +0300

    FoodStorage + ApiRequest

--- pinkipie/CoreData/CoreDataStack.swift
+++ pinkipie/CoreData/CoreDataStack.swift
@@ -12,7 +12,7 @@ class CoreDataStack{
     static let shared = CoreDataStack()
     
     private lazy var persistentContainer: NSPersistentContainer = {
-        let container = NSPersistentContainer(name: "pinky_example")
+        let container = NSPersistentContainer(name: "pink")
         container.loadPersistentStores(completionHandler: { (storeDescription, error) in
             if let error = error as NSError? {
                 fatalError("Unresolved error \(error), \(error.userInfo)")
